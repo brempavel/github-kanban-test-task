20df0781c96963cac81c07283b2ffe644e7c306e
Pavlo Brem <brempavel@gmail.com>
2024-03-26 22:30:55 +0200

    chore: add deploy script

--- client/src/store/api/boardsApi.ts
+++ client/src/store/api/boardsApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://kanban-board-ycsg.onrender.com';
 
 const boardsApi = createApi({
 	reducerPath: 'boards',
